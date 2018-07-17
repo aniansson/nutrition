72f0bfa3bf5d209693256a992946236628723df6
aniansson <ebba.aniansson@wikimedia.de>
2018-07-17 14:28:28 +0200

    Major commit, from pairing with Jonas

--- backend/controllers/IndexController.js
+++ backend/controllers/IndexController.js
@@ -7,7 +7,12 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/', function (request, response) {
-	var data = ['Fats', 'Fibres', 'Vitamins', 'Minerals', 'Water', 'Proteins', 'Carbohydrates'];
+	var data = {
+		Q44: 'Fats',
+		Q45: 'Fibres',
+		Q34956: 'Vitamins',
+	  /*'Vitamins', 'Minerals', 'Water', 'Proteins', 'Carbohydrates'*/
+	};
 	response.setHeader('Content-Type', 'application/json');
 	response.send(JSON.stringify(data));
 	response.status(200);
